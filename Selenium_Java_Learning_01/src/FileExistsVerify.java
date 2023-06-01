f1de3166d161ad5967973bfdef5cffa4a1343907
vsjanani <jansree90@gmail.com>
2023-05-31 19:13:48 -0700

    revision

--- Selenium_Java_Learning_01/src/FileExistsVerify.java
+++ Selenium_Java_Learning_01/src/FileExistsVerify.java
@@ -1,13 +1,20 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class FileExistsVerify {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.out.println(System.getProperty("user.dir"));
-		File f = new File(System.getProperty("user.dir")+"/src/Streams.java");
-		
+		File f = new File(System.getProperty("user.dir")+"/src/Streams.java");		
 		System.out.println(f.exists());
+		FileInputStream objFIS = new FileInputStream(System.getProperty("user.dir")+"/src/global.properties");
+		Properties objProp = new Properties();
+		objProp.load(objFIS);
+		System.out.println(objProp.getProperty("browser"));
+		
 			
 	}
 
--- Selenium_Java_Learning_01/src/TestngCheck.java
+++ Selenium_Java_Learning_01/src/TestngCheck.java
@@ -2,6 +2,7 @@ import org.testng.annotations.Test;
 
 public class TestngCheck {
 	
+	
 	@Test
 	public void testAnnotationCheck() {
 		System.out.println("it is working fine");
